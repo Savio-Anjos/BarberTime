05eedf2d41c0d7aa343cfb1834b9de2e812e1b7c
Savio Anjos <savioanjos2020@gmail.com>
2025-02-02 21:49:35 -0300

    feat: :sparkles: Added user repositories

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,28 @@
+import { Prisma, User } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+import { randomUUID } from "crypto";
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = [];
+  public async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    this.items.push(user);
+
+    return user;
+  }
+  public async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((user) => user.email === email);
+
+    if (!user) return null;
+
+    return user;
+  }
+}
---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,16 @@
+import { Prisma, User } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+import { prisma } from "@/lib/prisma";
+
+export class PrismaUsersRepository implements UsersRepository {
+  public async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = await prisma.user.create({ data });
+
+    return user;
+  }
+  public async findByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({ where: { email: email } });
+
+    return user;
+  }
+}
